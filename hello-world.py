3ecf631c081e881381aab6c0e3bed1c09376fad1
Stan Kozlov <dont@write.me>
2022-07-23 13:57:17 +1000

    refactor

--- hello-world.py
+++
@@ -1,9 +0,0 @@
-#--------------------------------------------------------------------------------------------
-#  Copyright (c) Red Hat, Inc. All rights reserved.
-#  Licensed under the MIT License. See LICENSE in the project root for license information.
-#--------------------------------------------------------------------------------------------
-
-# This program prints Hello, world!
-
-str = 'world'
-print('Hello, ' + str + '!')
